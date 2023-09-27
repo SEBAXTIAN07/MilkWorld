6861cbb23bd2892224a41a06e996f9ea2542602a
Juan Sebastian Forero <juanforeo@seti.com.co>
2023-09-27 16:20:50 -0500

    Login Realizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {StyleClassModule} from 'primeng/styleclass';
+import { RegisterComponent } from './register/register.component';
+import { ImageModule } from 'primeng/image';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StyleClassModule,
+    ImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
