385e1bebb005118218335a25b6bedbae3c105963
Juan Sebastian Forero <juanforeo@seti.com.co>
2023-10-25 17:30:51 -0500

    Registro Animal

--- src/app/finca/finca.component.ts
+++ src/app/finca/finca.component.ts
@@ -12,16 +12,23 @@ export class FincaComponent implements OnInit {
   stpe1: boolean = true; // Inicialmente visible
   stpe2: boolean = false; // Inicialmente visible
   stpe3: boolean = false; // Inicialmente visible
+  stpe4: boolean = false; // Inicialmente visible
+  stpe5: boolean = false; // Inicialmente visible
+  stpe6: boolean = false; // Inicialmente visible
 
   constructor(public messageService: MessageService) {}
 
   ngOnInit() {}
 
-  validarStpe(numero: number){
-    numero == 1 ? this.stpe1=true : this.stpe1=false;
-    numero == 2 ? this.stpe2=true : this.stpe2=false;
-    numero == 3 ? this.stpe3=true : this.stpe3=false;
+  validarStpe(numero: number) {
+    console.log(numero);
+    numero == 1 ? (this.stpe1 = true) : (this.stpe1 = false);
+    numero == 2 ? (this.stpe2 = true) : (this.stpe2 = false);
+    numero == 3 ? (this.stpe3 = true) : (this.stpe3 = false);
+    numero == 4 ? (this.stpe4 = true) : (this.stpe4 = false);
+    numero == 5 ? (this.stpe5 = true) : (this.stpe5 = false);
+    numero == 6 ? (this.stpe6 = true) : (this.stpe6 = false);
   }
 
-
+  
 }
